8e5fdb26940d5610a61f3c3f065d5bcbc2882bbb
HalfSuperNate <96854633+HalfSuperNate@users.noreply.github.com>
2023-12-02 20:24:43 -0600

    now supports ints and uints

--- Float.sol
+++ Float.sol
@@ -2,16 +2,32 @@
 pragma solidity ^0.8.23;
 
 contract Float {
-    function uintToFloat(uint256 number, uint256 decimals) external pure returns (string memory) {
+    function intToFloat(int256 number, uint256 decimals) external pure returns (string memory) {
         require(decimals <= 18, "Decimals too large");
-        uint256 prefix = number / (10**decimals);
-        uint256 suffix = number - (prefix * (10**decimals));
-        
-        return toFloat(prefix, suffix);
+        bool isNegative = number < 0;
+        uint256 absoluteValue = isNegative ? uint256(-number) : uint256(number);
+
+        uint256 divisor = 10**decimals;
+        uint256 prefix = absoluteValue / divisor;
+        uint256 suffix = absoluteValue % divisor;
+
+        string memory result = toFloat(prefix, suffix, decimals);
+
+        if (isNegative) {
+            result = string(abi.encodePacked("-", result));
+        }
+
+        return result;
     }
 
-    function toFloat(uint256 prefix, uint256 suffix) public pure returns (string memory) {
-        return string(abi.encodePacked(uintToString(prefix), ".", uintToString(suffix)));
+    function toFloat(uint256 prefix, uint256 suffix, uint256 decimals) public pure returns (string memory) {
+        string memory result = string(abi.encodePacked(uintToString(prefix)));
+
+        if (decimals > 0) {
+            result = string(abi.encodePacked(result, ".", padWithZeros(uintToString(suffix), decimals)));
+        }
+
+        return result;
     }
 
     function uintToString(uint256 value) internal pure returns (string memory) {
@@ -30,6 +46,24 @@ contract Float {
             buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
             value /= 10;
         }
+        return string(buffer);
+    }
+
+    function padWithZeros(string memory input, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(length);
+        bytes memory inputBytes = bytes(input);
+
+        uint256 i = 0;
+        while (i < length - inputBytes.length) {
+            buffer[i] = '0';
+            i++;
+        }
+
+        for (uint256 j = 0; j < inputBytes.length; j++) {
+            buffer[i] = inputBytes[j];
+            i++;
+        }
+
         return string(buffer);
     }
 }
